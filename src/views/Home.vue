77f979f27eb4fd2a9e6a7753d69707ef67d7976b
Yoni Hirshowitz <jonhirsh39@gmail.com>
2020-01-04 12:30:10 -0500

    many changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,75 @@
 <template>
-  <v-row>
-    <v-col cols="12" sm="6" offset-sm="3">
-      <v-card>
-        <v-container fluid>
-          <v-row>
-            <v-col
-              v-for="(photo, index) in photos"
-              v-bind:key="index"
-              class="d-flex child-flex"
-              cols="4"
+  <v-container fluid>
+     <v-data-iterator
+      :items="photos"
+      :photos-per-page.sync="photosPerPage"
+      :page="page"
+     
+      hide-default-footer
+    >
+    <template v-slot:default="props">
+      <v-row>
+        <v-col cols="12" sm="6" offset-sm="3">
+          <v-card>
+            <v-container fluid>
+              <v-row>
+                <v-col
+                  v-for="(photo, index) in props.photos"
+                  v-bind:key="index"
+                  class="d-flex child-flex"
+                  cols="4"
+                >
+                  <v-card flat tile class="d-flex">
+                    <v-img :src="photo.url" aspect-ratio="1" class="grey lighten-2" :id="photo.id">
+                      <template v-slot:placeholder>
+                        <v-row class="fill-height ma-0" align="center" justify="center">
+                          <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                        </v-row>
+                      </template>
+                    </v-img>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
+        </v-col>
+      </v-row>
+    </template>
+
+    <template>
+      <v-row class="mt-2" align="center" justify="center">
+        <span class="grey--text">Photos per page</span>
+        <v-menu offset-y>
+          <template v-slot:activator="{ on }">
+            <v-btn dark text color="primary" class="ml-2" v-on="on">
+              {{ photosPerPage }}
+              <v-icon>mdi-chevron-down</v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item
+              v-for="(number, index) in photosPerPageArray"
+              :key="index"
+              @click="updatephotosPerPage(number)"
             >
-              <v-card flat tile class="d-flex">
-                <v-img :src="photo.url" aspect-ratio="1" class="grey lighten-2" :id="photo.id">
-                  <template v-slot:placeholder>
-                    <v-row class="fill-height ma-0" align="center" justify="center">
-                      <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                    </v-row>
-                  </template>
-                </v-img>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card>
-    </v-col>
-  </v-row>
+              <v-list-item-title>{{ number }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+
+        <v-spacer></v-spacer>
+
+        <span class="mr-4 grey--text">Page {{ page }} of {{ numberOfPages }}</span>
+        <v-btn fab dark color="blue darken-3" class="mr-1" @click="formerPage">
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+        <v-btn fab dark color="blue darken-3" class="ml-1" @click="nextPage">
+          <v-icon>mdi-chevron-right</v-icon>
+        </v-btn>
+      </v-row>
+    </template>
+      </v-data-iterator>
+  </v-container>
 </template>
 
 <script>
@@ -35,6 +80,9 @@ export default {
   components: {},
   data() {
     return {
+      page: 1,
+      photosPerPage: 4,
+      photosPerPageArray: [4, 8, 12],
       photos: []
     };
   },
@@ -49,6 +97,22 @@ export default {
       };
       return photo;
     });
-  }
+  },
+  methods: {
+      nextPage () {
+        if (this.page + 1 <= this.numberOfPages) this.page += 1
+      },
+      formerPage () {
+        if (this.page - 1 >= 1) this.page -= 1
+      },
+      updatephotosPerPage (number) {
+        this.photosPerPage = number
+      },
+    },
+    computed: {
+      numberOfPages () {
+        return Math.ceil(this.photos.length / this.photosPerPage)
+      }
+    }
 };
 </script>
---
+++ src/views/Table.vue
@@ -0,0 +1,192 @@
+<template>
+  <v-container fluid>
+    <v-data-iterator
+      :items="items"
+      :items-per-page.sync="itemsPerPage"
+      :page="page"
+      :search="search"
+      :sort-by="sortBy.toLowerCase()"
+      :sort-desc="sortDesc"
+      hide-default-footer
+    >
+      <template v-slot:header>
+        <v-toolbar
+          dark
+          color="blue darken-3"
+          class="mb-1"
+        >
+          <v-text-field
+            v-model="search"
+            clearable
+            flat
+            solo-inverted
+            hide-details
+            label="Search"
+          ></v-text-field>
+          <template v-if="$vuetify.breakpoint.mdAndUp">
+            <v-spacer></v-spacer>
+            <v-select
+              v-model="sortBy"
+              flat
+              solo-inverted
+              hide-details
+              :items="keys"
+              label="Sort by"
+            ></v-select>
+            <v-spacer></v-spacer>
+            <v-btn-toggle
+              v-model="sortDesc"
+              mandatory
+            >
+              <v-btn
+                large
+                depressed
+                color="blue"
+                :value="false"
+              >
+                <v-icon>mdi-arrow-up</v-icon>
+              </v-btn>
+              <v-btn
+                large
+                depressed
+                color="blue"
+                :value="true"
+              >
+                <v-icon>mdi-arrow-down</v-icon>
+              </v-btn>
+            </v-btn-toggle>
+          </template>
+        </v-toolbar>
+      </template>
+
+      <template v-slot:default="props">
+        <v-row>
+          <v-col
+            v-for="item in props.items"
+            :key="item.id"
+            cols="12"
+            sm="6"
+            md="4"
+            lg="3"
+          >
+           <v-card flat tile class="d-flex">
+                    <v-img :src="item.url" aspect-ratio="1" class="grey lighten-2" :id="item.id">
+                      <template v-slot:placeholder>
+                        <v-row class="fill-height ma-0" align="center" justify="center">
+                          <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                        </v-row>
+                      </template>
+                    </v-img>
+                  </v-card>
+          </v-col>
+        </v-row>
+      </template>
+
+      <template v-slot:footer>
+        <v-row class="mt-2" align="center" justify="center">
+          <span class="grey--text">Items per page</span>
+          <v-menu offset-y>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                dark
+                text
+                color="primary"
+                class="ml-2"
+                v-on="on"
+              >
+                {{ itemsPerPage }}
+                <v-icon>mdi-chevron-down</v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item
+                v-for="(number, index) in itemsPerPageArray"
+                :key="index"
+                @click="updateItemsPerPage(number)"
+              >
+                <v-list-item-title>{{ number }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+
+          <v-spacer></v-spacer>
+
+          <span
+            class="mr-4
+            grey--text"
+          >
+            Page {{ page }} of {{ numberOfPages }}
+          </span>
+          <v-btn
+            fab
+            dark
+            color="blue darken-3"
+            class="mr-1"
+            @click="formerPage"
+          >
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+          <v-btn
+            fab
+            dark
+            color="blue darken-3"
+            class="ml-1"
+            @click="nextPage"
+          >
+            <v-icon>mdi-chevron-right</v-icon>
+          </v-btn>
+        </v-row>
+      </template>
+    </v-data-iterator>
+  </v-container>
+</template>
+
+<script>
+import photosArray from "@/assets/photos.js";
+  export default {
+    data () {
+      return {
+        itemsPerPageArray: [4, 8, 12],
+        items: [],
+        search: '',
+        filter: {},
+        sortDesc: false,
+        page: 1,
+        itemsPerPage: 4,
+        sortBy: "",
+        keys: [],
+      }
+    },
+    computed: {
+      numberOfPages () {
+        return Math.ceil(this.items.length / this.itemsPerPage)
+      },
+      filteredKeys () {
+        return this.keys.filter(key => key !== `Name`)
+      },
+    },
+    methods: {
+      nextPage () {
+        if (this.page + 1 <= this.numberOfPages) this.page += 1
+      },
+      formerPage () {
+        if (this.page - 1 >= 1) this.page -= 1
+      },
+      updateItemsPerPage (number) {
+        this.itemsPerPage = number
+      },
+    },
+  beforeMount() {
+    this.items = photosArray.map(photoUrl => {
+      let splitArray = photoUrl.split("/");
+      let item = {
+        url: photoUrl,
+        height: splitArray.slice(-1)[0],
+        width: splitArray.slice(-2)[0],
+        id: splitArray.slice(-3)[0]
+      };
+      return item;
+    });
+  }
+  }
+</script>
