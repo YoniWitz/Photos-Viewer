60754a336bb1f4c9ff911277c179029e1b5bfb02
Yoni Hirshowitz <jonhirsh39@gmail.com>
2020-01-05 17:49:26 -0500

    added signup page

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
+Vue.use(vuetify, {
+  iconfont:'mdi'
+})
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/views/Home.vue'
-import About from '@/views/About.vue'
-import Table from '@/views/Table.vue'
+import AboutVuetify from '@/views/AboutVuetify.vue'
+import Photos from '@/views/Photos.vue'
+import Login from '@/views/Login.vue'
+import Signup from '@/views/Signup.vue'
 
 Vue.use(VueRouter)
 
@@ -13,14 +15,24 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
+    path: '/about-vuetify',
     name: 'about',
-    component: About
+    component: AboutVuetify
   },
   {
-    path: '/table',
-    name: 'table',
-    component: Table
+    path: '/photos',
+    name: 'photos',
+    component: Photos
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: Signup
   }
 ]
 
