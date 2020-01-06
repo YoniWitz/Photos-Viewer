6ff3c815a3bb9e09e419af0e1d403042e820cef2
Yoni Hirshowitz <jonhirsh39@gmail.com>
2020-01-06 12:11:05 -0500

    add login logic

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,32 +1,18 @@
 <template>
   <v-app id="inspire">
     <v-content>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-row
-          align="center"
-          justify="center"
-        >
-          <v-col
-            cols="12"
-            sm="8"
-            md="4"
-          >
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
             <v-card class="elevation-12">
-              <v-toolbar
-                color="primary"
-                dark
-                flat
-              >
+              <v-toolbar color="primary" dark flat>
                 <v-toolbar-title>Login form</v-toolbar-title>
                 <v-spacer />
-                             
               </v-toolbar>
               <v-card-text>
                 <v-form>
                   <v-text-field
+                    v-model="email"
                     label="Login"
                     name="login"
                     prepend-icon="person"
@@ -34,17 +20,20 @@
                   />
 
                   <v-text-field
+                    v-model="password"
                     id="password"
                     label="Password"
                     name="password"
                     prepend-icon="lock"
                     type="password"
                   />
+                  <v-toolbar-title color="deep-purple" v-if="feedback">{{feedback}}</v-toolbar-title>
                 </v-form>
               </v-card-text>
               <v-card-actions>
                 <v-spacer />
-                <v-btn color="primary">Login</v-btn>
+
+                <v-btn color="primary" v-on:click="login">Login</v-btn>
               </v-card-actions>
             </v-card>
           </v-col>
@@ -55,9 +44,33 @@
 </template>
 
 <script>
-  export default {
-    props: {
-      source: String,
-    },
+import AuthService from "@/services/AuthServiceLogin";
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      feedback: ""
+    };
+  },
+  methods: {
+    async login() {
+      if (this.email && this.password) {
+        const response = await AuthService.login({
+          email: this.email,
+          password: this.password
+        })
+          .then(res => {
+            this.feedback = "";
+            console.log(res.data.msg  );
+          })
+          .catch(err => {
+            this.feedback = "some error occurred";
+          });
+      } else {
+        this.feedback = "must enter username and password";
+      }
+    }
   }
+};
 </script>
