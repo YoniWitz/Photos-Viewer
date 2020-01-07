4e99169277b6881863fcb9427f03694879710fcf
Yoni Hirshowitz <jonhirsh39@gmail.com>
2020-01-07 16:43:22 -0500

    added server files

---
+++ server/src/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const AuthController = require('./controller/AuthController')
+const PhotosController = require('./controller/PhotosController')
+
+const app = express()
+app.use(morgan('tiny'))
+app.use(bodyParser.json())
+app.use(cors())
+
+const port = 3009
+
+app.get('/photos/height/:height?/width/:width?/grayscale/:grayscale?', PhotosController.getPhotos)
+app.get('/photos', PhotosController.getAllPhotos)
+
+app.post('/register', AuthController.register)
+app.post('/login', AuthController.login)
+
+app.listen(port, console.log('listening'))
+
