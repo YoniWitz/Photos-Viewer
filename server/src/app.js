ae042ba4a9935814f14104e5ec2d870c2e06050e
Yoni Hirshowitz <jonhirsh39@gmail.com>
2020-01-09 20:43:44 -0500

    removed auth related files

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
-const AuthController = require('./controller/AuthController')
 const PhotosController = require('./controller/PhotosController')
 
 const app = express()
@@ -15,9 +14,6 @@ const port = 3009
 
 app.get('/photos/height/:height/width/:width/grayscale/:grayscale/page/:page/photosPerPage/:photosPerPage', PhotosController.getPhotos)
 app.get('/photos', PhotosController.getAllPhotos)
-
-app.post('/register', AuthController.register)
-app.post('/login', AuthController.login)
 
 app.listen(port, console.log('listening'))
 
